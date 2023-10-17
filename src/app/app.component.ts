e155c9ed1ec1e042afa742f07aa71899907f718c
Yelcde <johnnercastrojy@gmail.com>
2023-10-17 16:10:51 -0300

    Compartilhando dados entre componentes pai e filho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userName = "Joaquim";
+
+  userData = {
+    email: "joaquim@email.com",
+    telefone: '88988888888',
+    rua: 'Rua Seu Antônio'
+  };
+
   title = 'curso-angular';
 }
